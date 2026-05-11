fcec1f12ecd15da23287ac86a11907ceeae2b813
Raquel Almeida <raquelalmeida-15@outlook.pt>
2026-05-11 20:10:07 +0100

    Adicionar secção de edições e arquivo

---
+++ src/data/editions.js
@@ -0,0 +1,23 @@
+const editions = [
+  {
+    id: 1,
+    year: '2026',
+    subtitle: 'Edição Atual',
+    theme: 'navy',
+    image: 'hero-edicoes.png',
+  },
+  {
+    id: 2,
+    year: '2025',
+    subtitle: 'Ópera à Moda do Porto',
+    theme: 'orange',
+  },
+  {
+    id: 3,
+    year: '2024',
+    subtitle: 'Primeira edição do FIATO',
+    theme: 'navy',
+  },
+];
+
+export default editions;
