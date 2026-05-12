a69fe62de492d5dfd2019851a21b07322c055141
Raquel Almeida <raquelalmeida-15@outlook.pt>
2026-05-12 21:19:15 +0100

    edit agenda e edições

--- src/components/agenda-filters/agenda-filters.jsx
+++ src/components/agenda-filters/agenda-filters.jsx
@@ -2,17 +2,20 @@ import './agenda-filters.css';
 
 function AgendaFilters() {
   return (
-    <div className="agenda-filters">
+    <div className="agenda-filters" aria-label="Filtros da agenda">
       <button type="button" className="agenda-filters__button">
-        Título
+        <span className="agenda-filters__label">Filtrar por</span>
+        <span className="agenda-filters__value">Título</span>
       </button>
 
       <button type="button" className="agenda-filters__button">
-        Local
+        <span className="agenda-filters__label">Filtrar por</span>
+        <span className="agenda-filters__value">Local</span>
       </button>
 
       <button type="button" className="agenda-filters__button">
-        Data
+        <span className="agenda-filters__label">Filtrar por</span>
+        <span className="agenda-filters__value">Data</span>
       </button>
     </div>
   );
