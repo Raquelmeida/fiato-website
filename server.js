ba0c35bc1c50e7e57c3df47c5c07cfe70786eaaf
Raquel Almeida <raquelalmeida-15@outlook.pt>
2026-05-25 20:01:04 +0100

    Adicionar servidor Express para a versao HTML

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "v2")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "v2", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor a correr em http://localhost:${PORT}`);
+});
